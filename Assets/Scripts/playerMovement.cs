dde049f236151e0eaf12cfb1554181e4a77ce7a7
Dougal Scott <Dougal.Scott@gmail.com>
2023-01-19 18:02:28 +1100

    Part 6 complete

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,10 +7,12 @@ public class playerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
     private float dirX = 0f;
 
     [SerializeField] private float moveSpeed = 7.0f;
     [SerializeField] private float jumpForce = 14.0f;
+    [SerializeField] private LayerMask jumpableGround;
     private enum MovementState { idle, running, falling, jumping};
 
     // Start is called before the first frame update
@@ -19,6 +21,7 @@ public class playerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,7 +30,7 @@ public class playerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
 
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && onGround())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -57,5 +60,10 @@ public class playerMovement : MonoBehaviour
         }
 
         anim.SetInteger("state", (int) state);
+    }
+
+    private bool onGround() {
+        // Are we on the ground
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
 }
