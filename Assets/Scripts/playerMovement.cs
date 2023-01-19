999e3ec918ecd5b6c230fb58fc745468500f0ebe
Dougal Scott <Dougal.Scott@gmail.com>
2023-01-19 16:44:17 +1100

    Initial commit

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7.0f;
+    [SerializeField] private float jumpForce = 14.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0f) {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f) {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else {
+            anim.SetBool("running", false);
+        }
+    }
+}
